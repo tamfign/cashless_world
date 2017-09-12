cc79f0e8fdc8a751916d3c0d4a74d47f053b35bb
tamfign <tamfign@gmail.com>
2017-09-12 13:59:09 +1000

    Init main page

--- client_end/App.js
+++ client_end/App.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TabBarIOS, StatusBarIOS, Image, Text, View } from 'react-native';
+import Swiper from 'react-native-swiper';
+import NativeModules from 'NativeModules';
+
+import styles from '.src/styles';
+import Account from '.src/components/account';
+import QuickPay from '.src/components/quickpay';
+import Transaction from '.src/components/transaction';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<TabBarIOS tintColor="black" barTintColor="#3abeff">
+				<TabBarIOS.Item
+					icon=undefined
+					title="Account"
+					badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
+					selected={this.state.selectedTab === 'account'}
+					onPress={() => {
+						this.setState({selectedTab : 'account'});
+					}}>
+					<Account/>
+				</TabBarIOS.Item>
+
+				<TabBarIOS.Item
+					icon=undefined
+					title="Quick Pay"
+					selected={this.state.selectedTab === 'quickpay'}
+					onPress={() => {
+						this.setState({slectedTab : 'quickpay'})
+					}}>
+
+					<View style={styles.navigator}>
+						<QuickPay />
+					</View>
+				</TabBarIOS.Item>
+			</TabBarIOS>
+		);
+	}
+
+	getInitialState() {
+		return {
+			selectedTab : 'account',
+			notifCount : 1
+		};
+	}
 }
 
 const styles = StyleSheet.create({
