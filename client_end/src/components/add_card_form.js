f3fe5e3203a6f5a4a0f307ff8959398f74ef72e5
nazisangg <nazisang@gmail.com>
2017-09-18 18:27:08 +1000

    Save button of card

--- client_end/src/components/add_card_form.js
+++ client_end/src/components/add_card_form.js
@@ -1,23 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import styles from '../styles';
+
 import { CreditCardInput } from 'react-native-credit-card-input';
 
 export default class AddCardForm extends React.Component {
+
+	constructor() {
+		super();
+		this.state = {
+			form: Object
+		};
+	}
+
+	_onChange(form) {
+		this.state.form = form;
+	}
+
+	save_card() {
+		console.log(this.state.form);
+	}
+
 	render() {
 	return (
 		<View>
 			<Text style={styles.title}> Add New Card </Text>
 			<View>
 				<CreditCardInput
-					requiresName
 					requiresCVC
+					requiresName
 
 					labelStyle={styles.label}
 					inputStyle={styles.input}
 					validColor={"black"}
 					onFocus={this._onFocus}
-					onChange={this._onChange} />
+					onChange={this._onChange.bind(this)} />
+				<Button
+					title="Save"
+					onPress={this.save_card.bind(this)}/>
 			</View>
 		</View>
 	);
