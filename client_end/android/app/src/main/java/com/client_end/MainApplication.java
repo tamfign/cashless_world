9e122638087b1cc690fe80d22c7d950e89be1875
nazisangg <nazisang@gmail.com>
2017-09-26 21:52:55 +1000

    Remove android nfc module

--- client_end/android/app/src/main/java/com/client_end/MainApplication.java
+++ client_end/android/app/src/main/java/com/client_end/MainApplication.java
@@ -3,8 +3,6 @@ package com.client_end;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.novadart.reactnativenfc.ReactNativeNFCPackage;
-import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,9 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new ReactNativeNFCPackage(),
-            new RNGoogleSigninPackage()
+          new MainReactPackage()
       );
     }
   };
