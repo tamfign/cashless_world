f517037531a0161bb88a8f9e835a8a0b26dfb52f
nazisangg <nazisang@gmail.com>
2017-10-04 18:43:48 +1100

    Mering add card

---
+++ client_end/src/interfaces/add_card.js
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+
+export default class Transfer extends Component {
+
+	async addNewCards(userId, type, cardNumber, holder, expire, csv) {
+		try {
+			var response = await fetch('http://localhost:31415',{
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					UserId: userId,
+					Type: type,
+					CardInfo: {
+						CardNumber: cardNumber,
+						HolderName: holder,
+						ExpireDate: expire,
+						CSV: csv
+					}
+				})
+			});
+			var res = await response.text();
+			if(response.status>=200 && response.status<300) {
+				console.log("res success is:"+res);
+			} else {
+				throw res;
+			}
+		} catch(errors) {
+			console.log("error is:"+errors);
+		}
+	}
+}
