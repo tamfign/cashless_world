a658f3c94dacaf1a7f0b689e5ce13ef7eab5882f
Yinong Xia <nazisang@gmail.com>
2017-10-08 23:59:02 +1100

    add comments to client,tranlst and userinstance, delete httpserver

--- server/client.py
+++ server/client.py
@@ -1,44 +1,31 @@
 import socket
 import json
+# This file is used to test the live of server
+
+'''The configuration of client socket'''
 HOST = 'ec2-52-36-241-1.us-west-2.compute.amazonaws.com'
 PORT = 31415
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-dic3 = json.dumps({'Type': 'GetCardInfo', 'UserId':'WRS'})
-dic = json.dumps({'Type': 'TransferStart', 'UserId': 'xiayinong','Uuid':'1233211234567','Amount':'1','CardInfo':{'CardNumber': '001', 'HolderName':'002','ExpireDate':'003', 'CSV':'csv'}})
-dic2 = json.dumps({'Type': 'TransferEnd', 'UserId': 'xiayinong','Uuid':'1233211234567'})
+
+'''The message for testing'''
+dic_getcardinfo = json.dumps({'Type': 'GetCardInfo', 'UserId':'WRS'})
+dic_transferstart = json.dumps({'Type': 'TransferStart', 'UserId': 'xiayinong','Uuid':'1233211234567','Amount':'1','CardInfo':{'CardNumber': '001', 'HolderName':'002','ExpireDate':'003', 'CSV':'csv'}})
+dic_transferend = json.dumps({'Type': 'TransferEnd', 'UserId': 'xiayinong','Uuid':'1233211234567'})
+
+'''Testing loop'''
 while True:
     try:
-        print('Here')
         cmd = input("Please input msg:")
-        s.send(bytes(dic3, encoding = "utf8"))
+        s.send(bytes(dic_getcardinfo, encoding = "utf8"))
         data = s.recv(1024)
         print (data)
-        s.send(bytes(dic2, encoding="utf8"))
+        s.send(bytes(dic_transferstart, encoding="utf8"))
         data2 = s.recv(1024)
-        print (data2)
-        s.send(bytes(dic, encoding="utf8"))
+        print (data)
+        s.send(bytes(dic_transgerend, encoding="utf8"))
         data3 = s.recv(1024)
         print (data3)
     except BrokenPipeError:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((HOST, PORT))
-        while True:
-            try:
-                print('Here')
-                cmd = input("Please input msg:")
-                s.send(bytes(cmd, encoding="utf8"))
-                data = s.recv(1024)
-                print (data)
-                s.send(bytes(dic, encoding="utf8"))
-                data2 = s.recv(1024)
-                print (data2)
-                s.send(bytes('laile', encoding="utf8"))
-                data3 = s.recv(1024)
-                print (data3)
-            except BrokenPipeError:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect((HOST, PORT))
-
-    #s.close()
